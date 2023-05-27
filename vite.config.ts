6efc7e7e8aab36751cb4f71ab1035400a124834d
linhuizi <529246402@qq.com>
2023-05-27 14:45:45 +0800

    ✨  import deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,34 @@
-import { fileURLToPath, URL } from "node:url";
-
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { URL, fileURLToPath } from 'node:url'
+import UnoCSS from 'unocss/vite'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: "./",
+  plugins: [
+    vue(),
+    UnoCSS(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vitest',
+      ],
+
+      dts: 'src/auto-imports.d.ts',
+    }),
+    Components({
+      dirs: ['src/components'],
+      extensions: ['vue'],
+      include: [/\.vue$/, /\.vue\?vue/],
+      dts: 'src/components.d.ts',
+    })],
+  base: './',
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-});
+})
